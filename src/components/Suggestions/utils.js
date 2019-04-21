a96907a5c9c3e58ff641376f301ff7b7db2038ca
Kevin Cooper <cooperka92@gmail.com>
2019-04-20 21:39:43 -0400

    :sparkles: Add 3 kinds of emotion suggestions

--- src/components/Suggestions/utils.js
+++ src/components/Suggestions/utils.js
@@ -1,5 +1,10 @@
 import { relationships } from '../../suggestions/relationships';
 import { occupations } from '../../suggestions/occupations';
+import {
+  clearEmotions,
+  groupedEmotions,
+  emotionalDiads,
+} from '../../suggestions/emotions';
 
 const getRandomThing = (list) => () => {
   return list[Math.floor(Math.random() * list.length)];
@@ -13,5 +18,17 @@ export const suggestionTypes = {
   OCCUPATION: {
     name: 'Occupation',
     getRandomThing: getRandomThing(occupations),
+  },
+  EMOTION_SHORT: {
+    name: 'Emotion (short list)',
+    getRandomThing: getRandomThing(clearEmotions),
+  },
+  EMOTION_FULL: {
+    name: 'Emotion (full list)',
+    getRandomThing: getRandomThing(groupedEmotions),
+  },
+  EMOTIONAL_DIAD: {
+    name: 'Emotional diad',
+    getRandomThing: getRandomThing(emotionalDiads),
   },
 };
