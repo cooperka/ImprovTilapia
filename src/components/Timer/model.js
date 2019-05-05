1ed790e5b5081e774e252ae72dac4ff2e059b6c4
Kevin Cooper <cooperka92@gmail.com>
2019-05-05 18:15:54 -0400

    :package: Factor out initPersistedStore util

--- src/components/Timer/model.js
+++ src/components/Timer/model.js
@@ -1,32 +1,16 @@
-import * as _ from 'lodash';
-import { observable, autorun, toJS } from 'mobx';
-import { storeData, retrieveData } from '../../storageUtils';
+import { observable } from 'mobx';
+
+import { initPersistedStore } from '../../storageUtils';
 
 const STORAGE_KEY = 'TimerSettingsModel';
-
-/** Debounce slightly. */
-const PERSISTENCE_DELAY_MS = 100;
 
 export class TimerSettingsModel {
   @observable
   shouldIncreaseBrightness = true;
 
   constructor() {
-    retrieveData(STORAGE_KEY).then((savedValues) => {
-      // Set initial state if there's something valid saved.
-      if (_.isPlainObject(savedValues)) {
-        Object.assign(this, savedValues);
-      }
-
-      // Persist state any time it changes.
-      autorun(
-        async () => {
-          await storeData(STORAGE_KEY, toJS(this));
-        },
-        {
-          delay: PERSISTENCE_DELAY_MS,
-        },
-      );
+    initPersistedStore(this, STORAGE_KEY).catch((error) => {
+      console.error('Failed to initPersistedStore:', error);
     });
   }
 }
--- src/storageUtils.js
+++ src/storageUtils.js
@@ -1,4 +1,31 @@
+import * as _ from 'lodash';
 import { AsyncStorage } from 'react-native';
+import { autorun, toJS } from 'mobx';
+
+/** Debounce slightly. */
+const PERSISTENCE_DELAY_MS = 100;
+
+/**
+ * Given a MobX store, make it persist its values on change.
+ */
+export async function initPersistedStore(store, key) {
+  const savedValues = await retrieveData(key);
+
+  // Set initial state if there's something valid saved.
+  if (_.isPlainObject(savedValues)) {
+    Object.assign(store, savedValues);
+  }
+
+  // Persist state any time it changes.
+  autorun(
+    async () => {
+      await storeData(key, toJS(store));
+    },
+    {
+      delay: PERSISTENCE_DELAY_MS,
+    },
+  );
+}
 
 export async function storeData(key, data) {
   try {
