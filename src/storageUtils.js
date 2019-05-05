66649bcc08c8f3e81fa94fb053c795b4a6dab43a
Kevin Cooper <cooperka92@gmail.com>
2019-05-05 18:31:25 -0400

    :rocket: Add optional verbose storage logging

--- src/storageUtils.js
+++ src/storageUtils.js
@@ -5,6 +5,14 @@ import { autorun, toJS } from 'mobx';
 /** Debounce slightly. */
 const PERSISTENCE_DELAY_MS = 100;
 
+const VERBOSE = false;
+
+function logVerbose(...args) {
+  if (VERBOSE) {
+    console.debug(...args);
+  }
+}
+
 /**
  * Given a MobX store, make it persist its values on change.
  */
@@ -14,6 +22,7 @@ export async function initPersistedStore(store, key) {
   // Set initial state if there's something valid saved.
   if (_.isPlainObject(savedValues)) {
     Object.assign(store, savedValues);
+    logVerbose(`Initialized store for ${key}:`, savedValues);
   }
 
   // Persist state any time it changes.
@@ -30,6 +39,7 @@ export async function initPersistedStore(store, key) {
 export async function storeData(key, data) {
   try {
     await AsyncStorage.setItem(key, JSON.stringify(data));
+    logVerbose(`Stored data for ${key}:`, data);
   } catch (error) {
     console.error('Failed to storeData:', error);
   }
@@ -38,7 +48,9 @@ export async function storeData(key, data) {
 export async function retrieveData(key) {
   try {
     const data = await AsyncStorage.getItem(key);
-    return JSON.parse(data);
+    const parsed = JSON.parse(data);
+    logVerbose('Got data from storage:', parsed);
+    return parsed;
   } catch (error) {
     console.error('Failed to retrieveData:', error);
   }
